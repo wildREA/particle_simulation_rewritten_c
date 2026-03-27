db263d744d2eaa7f596b2f5e6be9c92dddd98828
Wild <wildrealinit@gmail.com>
2026-03-27 08:46:32 +0100

    refactor: Update exception codes in enum

--- include/exceptions.h
+++ include/exceptions.h
@@ -4,9 +4,11 @@
 typedef enum exception_codes {
   OK = 0,
   EOF_FAILURE = 1,
-  NO_RESULT = 2,
-  INVALID_TYPE = 3,
-  OUT_OF_BOUNDS = 4
+  LIMITED_ACCESS = 2,
+  NO_RESULT = 3,
+  INVALID_LENGTH = 4,
+  INVALID_TYPE = 5,
+  OUT_OF_BOUNDS = 6
 } exception_codes;
 
 void buf_helper(void);
